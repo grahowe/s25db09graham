60b74db928476e595bedef5b48fa89b719441eb6
Owen Graham <omgraham02@gmail.com>
2025-04-11 11:59:46 -0500

    Defined PUT functionality

--- controllers/instruments.js
+++ controllers/instruments.js
@@ -38,8 +38,24 @@ exports.instrument_delete = function(req,res) {
 	res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
 };
 
-exports.instrument_update_put = function(req, res) {
-	res.send('NOT IMPLEMENTED: Instrument update PUT' + req.params.id);
+exports.instrument_update_put = async function(req, res) {
+	console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`);
+	try {
+		let toUpdate = await Instrument.findById(req.params.id);
+		if(req.body.name) toUpdate.name = req.body.name;
+		if(req.body.type) toUpdate.type = req.body.type;
+		if(req.body.year) toUpdate.year = req.body.year;
+		let result = await toUpdate.save();
+		console.log("Success " + result);
+		res.send(result);
+		if(req.body.checkboxsale) toUpdate.sale = true;
+		else toUpdate.same = false;
+
+	}
+	catch(err) {
+		res.status(500);
+		res.send(`{"error":${err}:Update for id ${req.params.id} failed`);
+	}
 };
 
 exports.instrument_list = async function(req, res) {
--- routes/instruments.js
+++ routes/instruments.js
@@ -3,5 +3,7 @@ const instrument_controllers = require('../controllers/instruments');
 var router = express.Router();
 
 router.get('/instruments/:id', instrument_controllers.instrument_detail);
+router.post('/instruments/:id', instrument_controllers.instrument_create_post);
+router.put('/instruments/:id', instrument_controllers.instrument_update_put);
 
 module.exports = router;
